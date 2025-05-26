0afc566c11c64c50b12967c0642744c8d1c5b12f
ngonhattung <ngonhattung08062003@gmail.com>
2025-05-26 11:56:35 +0700

    Update cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:8022',
   // 'http://localhost:5173'
-  'http://127.0.0.1:8022',
-  'http://127.0.0.1:5173'
+  // 'http://127.0.0.1:8022',
+  // 'http://127.0.0.1:5173'
 ]
