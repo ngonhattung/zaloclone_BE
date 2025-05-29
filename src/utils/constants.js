8faa3564913cb6843574139bf5d6cc9b8f30c488
ngonhattung <ngonhattung08062003@gmail.com>
2025-05-29 16:17:19 +0700

    Update white list domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,7 @@ export const WHITELIST_DOMAINS = [
   // 'http://localhost:8022',
   // 'http://localhost:5173'
   'http://127.0.0.1:8022',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://web-chat-app-fe.vercel.app',
+  'https://www.alozola.online'
 ]
